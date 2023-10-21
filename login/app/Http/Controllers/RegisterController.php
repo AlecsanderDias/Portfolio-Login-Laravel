acbf427765f6c616915b9c4b4ac261673a5919d4
Alecsander <alecsanderdias@hotmail.com>
2023-10-20 23:40:52 -0300

    Criação de envio de email para cadastro

--- login/app/Http/Controllers/RegisterController.php
+++ login/app/Http/Controllers/RegisterController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterFormRequest;
+use App\Mail\ConfirmEmail;
 use App\Repositories\EloquentLoginRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -17,6 +19,8 @@ class RegisterController extends Controller
     public function store(RegisterFormRequest $request) {
         $login = $request->all();
         $login = $this->repository->createLogin($login);
-        return to_route('home')->with(['login' => $login]);
+
+        Mail::to($login->email)->send(new ConfirmEmail($login->username));
+        return to_route('home.index')->with(['login' => $login]);
     }
 }
---
+++ login/resources/views/email/confirmation.blade.php
@@ -0,0 +1,13 @@
+<x-mail::message>
+# Confirmação de Registro
+
+Este é um email para confirmar a sua conta registrada no sistema de Login.
+Usuário: {{ $username }}
+
+<x-mail::button :url="route('login.index')">
+Confirm Link
+</x-mail::button>
+
+Obrigado,<br>
+{{ config('app.name') }}
+</x-mail::message>
